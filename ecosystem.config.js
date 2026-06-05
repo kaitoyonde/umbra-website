4b54a5339c2e9d5d8ee62c0cf841c0eed2e71c8c
Akihiro Kaito <erlanggaibr2@gmail.com>
2026-06-06 02:58:02 +0700

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [
+      {
+        name: "go-air-app",
+        script: "air",            // PM2 executes the 'air' tool
+        interpreter: "none",      // Tells PM2 it's a binary executible, not a JS file
+        merge_logs: true,         // Aggregates standard logs
+        env: {
+          NODE_ENV: "production",
+          // Ensure your system Go paths are accessible by PM2
+          PATH: process.env.PATH + ":" + process.env.GOPATH + "/bin"
+        }
+      }
+    ]
+  };
